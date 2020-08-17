4ebb1c9eb233536aa30af6889b23baae7fac5e36
adetiamarhadi <adetia.marhadi@gmail.com>
2020-08-17 14:41:55 +0700

    demo spring cloud hystrix with turbine

---
+++ src/test/java/com/github/adetiamarhadi/springcloudhystrixturbine/SpringCloudHystrixTurbineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.adetiamarhadi.springcloudhystrixturbine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudHystrixTurbineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
